c645a2f2d8610fcd6230a1afec66621933030f98
Le Quang Minh <minhle.91719@gmail.com>
2019-04-05 21:02:39 +0700

    fix querybuilder

---
+++ insert_test.go
@@ -0,0 +1,30 @@
+package mquery
+
+import "testing"
+
+func Test_insertQueryBuilder_ToQuery(t *testing.T) {
+	type fields struct {
+		tableName string
+		col       []string
+		value     []interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			iqb := &insertQueryBuilder{
+				tableName: tt.fields.tableName,
+				col:       tt.fields.col,
+				value:     tt.fields.value,
+			}
+			if got := iqb.ToQuery(); got != tt.want {
+				t.Errorf("insertQueryBuilder.ToQuery() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- query.go
+++ query.go
@@ -1,13 +1,18 @@
 package mquery
 
 type QueryBuilder interface {
-	Table(name string) QueryBuilder
 	InsertBuilder() InsertQueryBuilder
 	SelectBuilder() SelectQueryBuilder
 	// UpdateBuilder()
 }
 type queryBuilder struct {
 	tableName string
+}
+
+func NewTable(name string) QueryBuilder {
+	return &queryBuilder{
+		tableName: name,
+	}
 }
 
 func (qb *queryBuilder) Table(name string) QueryBuilder {
